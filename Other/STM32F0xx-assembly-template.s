0a677658b90103b89d87b1d047300cc0e6dfad95
WoodyWoodsta <sean@anotherway.co.za>
2014-08-01 13:59:22 +0200

    initial commit

---
+++ Other/STM32F0xx-assembly-template.s
@@ -0,0 +1,14 @@
+
+
+; This is a typical assembly code template for programming
+; the STM32F051C6 MCU
+
+
+
+  .syntax unified
+  .cpu cortex-m0
+  .thumb
+  .global _start
+
+  .word 0x20001FFF
+  .word _start + 1
