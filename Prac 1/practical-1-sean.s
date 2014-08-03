b326445a2fd64143375962e03b6412b7cb69efe7
WoodyWoodsta <sean@anotherway.co.za>
2014-08-03 20:23:21 +0200

    progress

--- Prac 1/practical-1-sean.s
+++ Prac 1/practical-1-sean.s
@@ -1,11 +1,14 @@
+@ WDXSEA003 CHTREE002
+
   .syntax unified
   .cpu cortex-m0
-  .thumb
   .global _start
 
   .word 0x20001FFF
   .word _start + 1
   
-_start: MOVS R1, #7
-  LDR R1, [R0, #4]
+_start: 
+  MOVS R0, #0x01
 
+@copy_to_RAM_complete:
+@infinite_loop:
