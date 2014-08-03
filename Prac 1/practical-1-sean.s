78a81af18db205070bb55c94b4c7fa90325c9bd6
WoodyWoodsta <sean@anotherway.co.za>
2014-08-03 20:36:59 +0200

    test LDR instruction

--- Prac 1/practical-1-sean.s
+++ Prac 1/practical-1-sean.s
@@ -8,7 +8,14 @@
   .word _start + 1
   
 _start: 
-  MOVS R0, #0x01
+  MOVS R0, #0x20000000
+  MOVS R1, A
+  LDR R1, [R0, #0]
 
 @copy_to_RAM_complete:
 @infinite_loop:
+
+A: .word 0xAABBCCDD
+B: .word 0x00001122
+C: .word 0x00002233
+D: .word 0x55555555
