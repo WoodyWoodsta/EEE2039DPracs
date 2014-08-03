4bbcdf4573123de5b64317cb8d5115ff44ec7637
WoodyWoodsta <sean@anotherway.co.za>
2014-08-03 23:20:52 +0200

    implement all operations
    loop doesn't exactly work

--- Prac 1/practical-1-sean.s
+++ Prac 1/practical-1-sean.s
@@ -17,17 +17,35 @@ _start:
   LDR R1, D
   STR R1, [R0, #12]
 
-@copy_to_RAM_complete:
-  LDR R1, [R0]
+copy_to_RAM_complete:
+  LDR R1, [R0]          @Operation: A - B
   LDR R2, [R0, #4]
   SUBS R1, R1, R2
+  STR R1, [R0, #16]
+  
+  LDR R1, [R0]          @Operation: A + B + C
+  LDR R2, [R0, #4]
+  ADDS R1, R1, R2
+  LDR R2, [R0, #8]
+  ADDS R1, R1, R2
+  STR R1, [R0, #20]
+
+  LDR R1, [R0, #4]      @Operation: B XOR C
+  LDR R2, [R0, #8]
+  EORS R1, R1, R2
+  STR R1, [R0, #24]
+
+  LDR R1, [R0, #4]      
+  MULS R1, R1, R2
+  STR R1, [R0, #28]
 
 
 @infinite_loop:
+  B infinite_loop
 
   .align
-RAM_START: .word 0x20000000
-A: .word 0xAABBCCDD
-B: .word 0x00001122
-C: .word 0x00002233
-D: .word 0x55555555
+  RAM_START: .word 0x20000000
+  A: .word 0xAABBCCDD
+  B: .word 0x00001122
+  C: .word 0x00002233
+  D: .word 0x55555555
