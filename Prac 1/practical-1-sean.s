e6fafd26e33f99acec923c985da7abfe22b2cd24
WoodyWoodsta <sean@anotherway.co.za>
2014-08-04 17:34:53 +0200

    fixed writing to GPIOB registers
    had to turn on the RCC clock via RCC_AHBENR register

--- Prac 1/practical-1-sean.s
+++ Prac 1/practical-1-sean.s
@@ -35,16 +35,22 @@ copy_to_RAM_complete:
   EORS R1, R1, R2
   STR R1, [R0, #24]
 
-  LDR R1, [R0, #4]      
+  LDR R1, [R0, #4]      @Operation: B * C
   MULS R1, R1, R2
   STR R1, [R0, #28]
 
-  LDR R0, PORTB_START
-  LDR R1, PORT_MODE1
-  LDR R4, [R0, #32]
-  ORRS R1, [R0, #0]
-  LDR R1, PORT_OUTPUT1
-  STR R1, [R0, 0x14]
+  LDR R0, RCC_START                @Specify start of RCC register
+  LDR R1, [R0, #20]                @Load RCC_AHBENR value
+  LDR R2, RCC_AHBENR_GPIOBEN       @Load RCC_AHBENR_GPIOBEN value
+  ORRS R1, R1, R2                  @OR values for RCC_AHBENR register
+  STR R1, [R0, #20]                @Reload RCC_AHBENR register
+  LDR R0, PORTB_START              @Specify start of GPIOB register addresses
+@ LDR R3, [R0]                     @Just check what is in here
+  LDR R1, PORT_MODE1               @Specify output mode to Digital Output (01)
+  STR R1, [R0]                     @Set output mode of GPIOB
+@ LDR R4, [R0, 0x14]               @Just check what is in there
+  LDR R1, PORT_OUTPUT1             @Specifiy data
+  STR R1, [R0, #20]                @Write data
 
 
 infinite_loop:
@@ -57,5 +63,7 @@ A: .word 0xAABBCCDD
 B: .word 0x00001122
 C: .word 0x00002233
 D: .word 0x55555555
-PORT_MODE1: .word 0b1010101010101010
-PORT_OUTPUT1: .word 0b111
+RCC_START: .word 0x40021000
+RCC_AHBENR_GPIOBEN: .word 0x00040000
+PORT_MODE1: .word 0x00005555
+PORT_OUTPUT1: .word 0b11
