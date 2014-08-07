25565d94e6af9798c38bdff4c721ffc6063159a1
WoodyWoodsta <sean@anotherway.co.za>
2014-08-07 23:30:36 +0200

    configure port B

--- Prac 2/prac-2-sean.s
+++ Prac 2/prac-2-sean.s
@@ -8,4 +8,18 @@
   .word 0x20001FFF
   .word _start + 1
 
-  
+_start:
+  LDR R0, RCC_START                             @Enable PORT B
+  LDR R1, RCC_AHBENR_GPIOBEN
+  STR R1, [R0, #20]
+  LDR R0, PORTB_START                           @Set PORT B to Output
+  LDR R1, PORTB_MODEROUT
+  STR R1, [R0]
+
+
+
+  .align
+RCC_START: .word 0x40021000
+RCC_AHBENR_GPIOBEN: .word 0x00040000
+PORTB_START: .word 0x48000400
+PORTB_MODEROUT: .word 0x00005555
