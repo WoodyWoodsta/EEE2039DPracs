ab92b808ea4d84e2ef74e63733d05bf70bfa7ffc
WoodyWoodsta <sean@anotherway.co.za>
2014-08-09 00:06:17 +0200

    finish up required part of prac

--- Prac 2/prac-2-sean.s
+++ Prac 2/prac-2-sean.s
@@ -11,16 +11,31 @@
 _start:
   LDR R0, RCC_START                             @Enable PORT B
   LDR R1, RCC_AHBENR_GPIOBEN
+  LDR R2, [R0, #20]
+  ORRS R1, R1, R2
   STR R1, [R0, #20]
   LDR R0, PORTB_START                           @Set PORT B to Output
   LDR R1, PORTB_MODEROUT
   STR R1, [R0]
 
-_all_off:                                       @Turn all LEDs off
-  LDR R1, 0x0
+all_off:                                        @Turn all LEDs off
+  MOVS R1, 0b00000000
   STR R1, [R0, 0x14]
 
-  
+display_AA:
+  MOVS R1, 0xAA                                 @Display 0xAA on the LEDs
+  STR R1, [R0, 0x14]
+
+all_on:
+  MOVS R1, 0b11111111                           @Turn all LEDs on
+  STR R1, [R0, 0x14]
+
+bonus:
+
+
+end:
+  B all_off                                     @Loop through everything
+
 
   .align
 RCC_START: .word 0x40021000
