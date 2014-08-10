1aff8d1b14027df3e9937178cfbf1e7009b0b62e
WoodyWoodsta <sean@anotherway.co.za>
2014-08-10 20:39:16 +0200

    fixed button input

--- Prac 2/prac-2-sean.s
+++ Prac 2/prac-2-sean.s
@@ -30,12 +30,22 @@ all_on:
   MOVS R1, 0b11111111                           @Turn all LEDs on
   STR R1, [R0, 0x14]
 
-bonus:
-  LDR R4, PORTA_START
-  LDR R5, [R4, 0x10]
-  MOVS R1, 0x0
-  ADDS R1, R5, R1
-  STR R1, [R0, 0x14]
+bonus_init:
+  LDR R1, PORTA_START
+  LDR R2, PORTA_PUPDR            @Pullup mode for PA0-3
+  LDR R3, [R1, 0x0C]
+  ORRS R2, R2, R3
+  STR R2, [R1, 0x0C]
+  LDR R2, PORTA_MODERIN          @Input mode for PA0-3 and output for the rest  
+  LDR R3, [R1]
+  ORRS R2, R2, R3
+  STR R2, [R1]
+
+bonus:                    @Take GPIOA_IDR and display it
+  LDR R2, [R1, 0x10]
+  MOVS R3, 0x0
+  ADDS R2, R2, R3
+  STR R2, [R0, 0x14]
   B bonus
 
 
@@ -49,3 +59,5 @@ RCC_AHBENR_GPIO_AB_EN: .word 0x00060000
 PORTB_START: .word 0x48000400
 PORTA_START: .word 0x48000000
 PORTB_MODEROUT: .word 0x00005555
+PORTA_MODERIN: .word 0x5500
+PORTA_PUPDR: .word 0x55
