e0b2122707b53bb54644aca41f115c37c9453d8b
WoodyWoodsta <sean@anotherway.co.za>
2014-08-31 09:50:49 +0200

    start TC74A0 sensor read implement

--- Prac 4/prac-4-sean.s
+++ Prac 4/prac-4-sean.s
@@ -12,7 +12,7 @@ vectors:
 _start:
   LDR R0, RCC_START                                               @Enable GPIOA and GPIOB RCC Clock
   LDR R2, [R0, 0x14]
-  LDR R3, RCC_AHBENR_GPIO_AB_EN
+  LDR R3, RCC_AHBENR_GPIO_ABEN
   ORRS R2, R2, R3
   STR R2, [R0, 0x14]
 
@@ -90,6 +90,7 @@ increment_of_bytes_complete:
   MOVS R4, R0                                                     @Relocate the smallest byte 
   MOVS R5, R1                                                     @Relocate the largest byte
   MOVS R6, R2                                                     @Relocate the largest signed byte
+  BL iic_Init_A
   LDR R0, PORTA_START                                             @Load base address for Ports A and B  |
   LDR R1, PORTB_START                                             @PORT A - [R0], PORT B - [R1]         |
   LDR R2, [R0, 0xC] 
@@ -104,6 +105,8 @@ increment_of_bytes_complete:
   STR R2, [R1]
 
 sw_Check:
+  LDR R0, PORTA_START                                             @Load base address for Ports A and B  |
+  LDR R1, PORTB_START                                             @PORT A - [R0], PORT B - [R1]         |
   LDR R2, [R0, 0x10]                                              @Load and mask SW states              |
   LDR R7, =0b111                                                  @                                     |
   ANDS R2, R2, R7                                                 @                                     |
@@ -114,6 +117,8 @@ sw_Check:
   BEQ display_Min
   CMP R2, #2
   BEQ display_MaxS
+  CMP R2, #4
+  BEQ display_Temp
 
 display_Max:
   STR R5, [R1, 0x14]
@@ -127,16 +132,225 @@ display_MaxS:
   STRB R6, [R1, 0x14]
   B sw_Check
 
+display_Temp2:
+  LDR R3, =0b010101
+  STR R3, [R1, 0x14]
+  B sw_Check
+
+iic_Init_A:
+  @Enable PORT F RCC Clock
+  LDR R0, RCC_START
+  LDR R1, RCC_AHBENR_GPIO_FEN
+  LDR R2, [R0, 0x14]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x14]
+  @Set SCLK (PORT F, Pin 6) output type to OPEN, DRAIN
+  LDR R0, PORTF_START
+  LDR R1, GPIO_OTYPER_OT_6
+  LDR R2, [R0, 0x04]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x04]
+  @Set SCLK (PORT F, Pin 6) mode to OUTPUT
+  LDR R1, GPIO_MODER_MODER6_0
+  LDR R2, [R0]
+  ORRS R2, R2, R1
+  STR R2, [R0]
+  LDR R7, =0x15
+  B slave_Clockout_A
+
+slave_Clockout_A: @Main clockout loop
+  LDR R3, =0x0B
+  SUBS R7, #1
+  CMP R7, #0
+  BNE slave_Clockout_B1
+  B iic_Init_B
+
+slave_Clockout_B1:
+  SUBS R3, #1
+  CMP R3, #0
+  BNE slave_Clockout_B1
+  LDR R3, =0x0B
+  LDR R1, GPIO_BSRR_BR_6
+  LDR R2, [R0, 0x18]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x18]
+  B slave_Clockout_B2
+
+slave_Clockout_B2:
+  SUBS R3, #1
+  CMP R3, #0
+  BNE slave_Clockout_B2
+  LDR R1, GPIO_BSRR_BS_6
+  LDR R2, [R0, 0x18]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x18]
+  B slave_Clockout_A
+
+
+iic_Init_B:
+  @Enable PORT F RCC Clock
+  LDR R0, RCC_START
+  LDR R1, RCC_AHBENR_GPIO_FEN
+  LDR R2, [R0, 0x14]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x14]
+
+  @Set SCLK (PORT F, Pin 6) mode to ALTERNATE FUNCTION
+  LDR R0, PORTF_START
+  LDR R1, GPIO_MODER_MODER6_1
+  LDR R2, [R0]
+  ORRS R2, R2, R1
+  STR R2, [R0]
+  @Set SCLK (PORT F, Pin 6) output type to OPEN, DRAIN
+  LDR R1, GPIO_OTYPER_OT_6
+  LDR R2, [R0, 0x4]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x4]
+
+  @Set SDA (PORT F, Pin 7) mode to ALTERNATE FUNCTION
+  LDR R1, GPIO_MODER_MODER7_1
+  LDR R2, [R0]
+  ORRS R2, R2, R1
+  STR R2, [R0]
+  @Set SCLK (PORT F, Pin 6) output type to OPEN, DRAIN
+  LDR R1, GPIO_OTYPER_OT_7
+  LDR R2, [R0, 0x4]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x4]
+
+  @Enable I2C2 RCC Clock
+  LDR R0, RCC_START
+  LDR R1, RCC_APB1ENR_I2C2EN
+  LDR R2, [R0, 0x1C]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x1C]
+
+  @Disable I2C Peripheral (Using control register 1)
+  LDR R0, I2C2_START
+  LDR R1, I2C_CR1_PE
+  LDR R3, =0x1
+  EORS R1, R1, R3
+  LDR R2, [R0]
+  ANDS R2, R2, R1
+  STR R2, [R0]
+
+  @Set timing of the following in TIMINGR
+  @SCLL
+  LDR R1, =0xC7
+  LSLS R1, #0
+  LDR R2, [R0, 0x10]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x10]
+  @SCLH
+  LDR R1, =0xC3
+  LSLS R1, #8
+  LDR R2, [R0, 0x10]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x10]
+  @SDADEL
+  LDR R1, =0x02
+  LSLS R1, #16
+  LDR R2, [R0, 0x10]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x10]
+  @SCLDEL
+  LDR R1, =0x04
+  LSLS R1, #20
+  LDR R2, [R0, 0x10]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x10]
+  @PRESC
+  LDR R1, =0x1
+  LSLS R1, #28
+  LDR R2, [R0, 0x10]
+  ORRS R2, R2, R1
+  STR R2, [R0, 0x10]
+
+  @Enable I2C Peripheral
+  LDR R1, I2C_CR1_PE
+  LDR R2, [R0]
+  ORRS R2, R2, R1
+  STR R2, [R0]
+
+  BX LR
+
+
+display_Temp:
+  LDR R2, I2C2_START
+  @Set slave address in SADD (Control Register 2)
+  LDR R3, TC74ADDR_READ
+  LDR R7, [R2, 0x4]
+  ORRS R7, R7, R3
+  STR R7, [R2, 0x4]
+  @Set Number of bytes to 1
+  LDR R3, =0x1
+  LSLS R3, #16
+  LDR R7, [R2, 0x4]
+  ORRS R7, R7, R3
+  STR R7, [R2, 0x4]
+  @Indicate read action
+  LDR R3, I2C_CR2_RD_WRN
+  LDR R7, [R2, 0x4]
+  ORRS R7, R7, R3
+  STR R7, [R2, 0x4]
+  @Send START
+  LDR R3, I2C_CR2_START
+  LDR R7, [R2, 0x4]
+  ORRS R7, R7, R3
+  STR R7, [R2, 0x4]
+
+  BL wait_TC
+
+  @Display TEMP data!!
+  LDR R3, [R2, 0x24]
+  STR R3, [R1, 0x14]
+
+  @Send STOP
+  LDR R3, I2C_CR2_STOP
+  LDR R7, [R2, 0x4]
+  ORRS R7, R7, R3
+  STR R7, [R2, 0x4]
+
+  B sw_Check
+
+
+wait_TC: @Wait for ACK and TC Flag
+  LDR R3, [R2, 0x18]
+  LDR R7, I2C_ISR_RXNE
+  ANDS R7, R7, R3
+  CMP R7, #0
+  BNE wait_TC
+  BX LR
+
+
   .align
 
 @ Program Variables
 RCC_START: .word 0x40021000
-RCC_AHBENR_GPIO_AB_EN: .word 0x00060000
+RCC_AHBENR_GPIO_ABEN: .word 0x00060000
+RCC_AHBENR_GPIO_FEN: .word 0x00400000
+RCC_APB1ENR_I2C2EN: .word 0x00400000
 PORTA_START: .word 0x48000000
 PORTA_MODERIN: .word 0x28000000
 PORTA_PUPDR: .word 0x55
 PORTB_START: .word 0x48000400
 PORTB_MODEROUT: .word 0x00005555
+PORTF_START: .word 0x48001400
+GPIO_MODER_MODER6_0: .word 0x00001000
+GPIO_MODER_MODER6_1: .word 0x00002000
+GPIO_OTYPER_OT_6: .word 0x00000040
+GPIO_MODER_MODER7_1: .word 0x00008000
+GPIO_OTYPER_OT_7: .word 0x00000080
+I2C2_START: .word 0x40005800
+I2C_CR1_PE: .word 0x00000001
+TC74ADDR_READ: .word 0b10010001
+TC74ADDR_WRITE: .word 0b10010000
+I2C_CR2_RD_WRN: .word 0x00000400
+I2C_CR2_START: .word 0x00002000
+I2C_ISR_RXNE: .word 0x00000004
+I2C_CR2_STOP: .word 0x00004000
+GPIO_BSRR_BR_6: .word 0x00400000
+GPIO_BSRR_BS_6: .word 0x00000040
 RAM_START: .word 0x20000000
 
 @ Data
